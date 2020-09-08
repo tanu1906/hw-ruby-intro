97f933a5b7482033932c405a2a1fdeae42f60e5e
EC2 Default User <ec2-user@ip-172-31-17-231.us-east-2.compute.internal>
2020-09-08 05:07:50 +0000

    SEhomework

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,77 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  if arr.length > 0
+        arr.inject {|add,n| add+n}
+  else
+        0
+  end
+
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  n = arr.length
+  if n == 0
+    0 
+  elsif n == 1
+    arr[0]
+  else
+    x = arr.sort.reverse
+    x[0] + x[1]    
+  end
+
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.length > 0 
+    m = arr.combination(2).find{|x,y| x + y == n}
+    m != nil
+  else
+    false
+  end
+
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  !!(s[0] =~ /[b-df-hj-np-tv-z]/i)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+    if !!(s =~ /[^01]/) == true || s.length == 0
+      false
+    else
+      if s.to_i(2) % 4 == 0
+        true
+      else
+        false
+      end
+    end
 end
+
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize(is, pr)
+    pr = pr.to_f
+    if is.length != 0 && pr > 0
+      @isbn = is
+      @price = pr
+    else
+      raise ArgumentError.new("Incorrect argument")
+    end
+  end
+  
+  def price_as_string
+    return "$" + format("%.2f", @price)
+  end
+
 end
